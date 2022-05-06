dc3f4c275f7ee6ecb203d6d64393d00180839427
Carlos Marx <carlosmarx27@gmail.com>
2022-05-06 08:57:47 -0300

    Add axios and adjust send data to backend

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from "phosphor-react"
 import { FormEvent, useState } from "react";
 import { FeedbackType , feedbackTypes} from ".."
+import { api } from "../../../libs/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -18,16 +20,22 @@ export function FeedbackContentStep({
 
 const [screenshot, setScreenshot] = useState<string | null>(null)
 const [comment, setComment] = useState<string>('')
+const [isSendingFeedback, setIsSendingFeedback] = useState<boolean>(false)
 
 const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-    function handleSubmitFeedback(event: FormEvent){
+    async function handleSubmitFeedback(event: FormEvent){
       event.preventDefault()
 
-      console.log({
-        screenshot,
-        comment
+      setIsSendingFeedback(true)
+
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        comment,
+        screenshot
       })
+
+      setIsSendingFeedback(false)
 
       onFeedbackSent()
     }
@@ -60,10 +68,10 @@ const feedbackTypeInfo = feedbackTypes[feedbackType]
           />
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:bg-brand-500"
           >
-            Enviar feedback
+            { isSendingFeedback ? <Loading /> : 'Enviar feedback' }
           </button>
         </footer>
       </form>
